2eab922a3d191276ebee8dd6e1f81974d556e21e
Shihab Hasan <shihabbd018@gmail.com>
2023-10-18 16:16:38 +0600

    added product get route and mongdb client

--- index.js
+++ index.js
@@ -10,11 +10,47 @@ const port = process.env.PORT || 9000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kitiq8p.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+const connectDB = async () => {
+    try {
+        await client.connect();
+
+        const productCollection = client.db("productDB").collection("products");
+
+
+
+        //GET products
+        app.get("/products", async (req, res) => {
+            const result = await productCollection.find().toArray();
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+
+    }
+}
+connectDB().catch(console.dir);
 
 
 
