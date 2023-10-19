00fe837113a87c2f1780c2f0de80316348db4622
Shihab Hasan <shihabbd018@gmail.com>
2023-10-19 12:14:18 +0600

    added routes for cart item delete operation

--- index.js
+++ index.js
@@ -95,6 +95,15 @@ const connectDB = async () => {
 
 
 
+        //DELETE cart item
+        app.delete("/cart/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: id };
+            const result = await cartCollection.deleteOne(query)
+            res.send(result);
+        });
+
+
 
         //GET sliders
         app.get("/sliders", async (req, res) => {
